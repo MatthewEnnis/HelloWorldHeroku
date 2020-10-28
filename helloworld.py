46498d49a560ee26a28ac66191a15c7e962d70d2
MatthewEnnis <mattennis123@gmail.com>
2020-10-28 19:19:07 +0000

    Made it a little fancier

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return '<p style="text-align:center;font-size:10em">Hello World!</p>'
+	return  """
+			<title>Hello World!</title>
+			<body onmousemove='moveText(event)'>
+			<script>
+			function moveText(e) {
+			document.getElementsByTagName("p")[0].style.wordSpacing=e.clientX*-.3;
+			}
+			</script>
+			<p style='text-align:center;font-size:15vw;word-spacing:-15vw'>
+			<span style='color:#EB001BFF'>Hello </span>
+			<span style='color:#F79E1BCC'>World</span>
+			</p>
+			</body>
+			"""
 
 if __name__ == "__main__":
 	app.run()
